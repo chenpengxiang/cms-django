df9e7bf6d58db9e39d66eb5b201ed2bdfcbd4c81
Quy Nguyen <v-quynnguyen@expedia.com>
2019-06-06 13:04:45 +0800

    Initial side by side

---
+++ backend/cms/views/pages/sbs_page.py
@@ -0,0 +1,142 @@
+"""
+    Side by side view
+"""
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.utils.translation import ugettext as _
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+
+from cms.views.pages.sbs_page_form import SBSPageForm
+from ...models import Page, Site, Language
+
+
+@method_decorator(login_required, name='dispatch')
+class SBSPageView(TemplateView):
+    template_name = 'pages/sbs_page.html'
+    base_context = {'current_menu_item': 'pages'}
+
+    def get(self, request, *args, **kwargs):
+        site = Site.objects.get(slug=kwargs.get('site_slug'))
+        page = Page.objects.filter(pk=kwargs.get('page_id', None)).first()
+        source_language_code, target_language_code = kwargs.get('language_code').split('__')
+        source_language = Language.objects.get(code=source_language_code)
+        target_language = Language.objects.get(code=target_language_code)
+        languages = site.languages
+        # limit possible parents to pages of current region
+        parent_queryset = Page.objects.filter(
+            site__slug=site.slug
+        )
+        initial = {}
+        public = False
+
+        if page:
+            initial['parent'] = page.parent
+            # remove children from possible parents
+            children = page.get_descendants(include_self=True)
+            parent_queryset = parent_queryset.difference(children)
+            page_translation = page.get_translation(source_language.code)
+            if page_translation:
+                initial.update({
+                    'title': page_translation.title,
+                    'text': page_translation.text,
+                    'status': page_translation.status,
+                    'public': page_translation.public,
+                })
+                public = page_translation.public
+        form = SBSPageForm(initial=initial)
+        form.fields['parent'].queryset = parent_queryset
+
+        return render(request, self.template_name, {
+            'form': form,
+            'public': public,
+            'page': page,
+            'source_language': source_language,
+            'target_language': target_language,
+            'languages': languages,
+            **self.base_context,
+        })
+
+    def post(self, request, *args, **kwargs):
+        site_slug = kwargs.get('site_slug')
+        page_id = kwargs.get('page_id', None)
+        language_code = kwargs.get('language_code')
+        # TODO: error handling
+        form = SBSPageForm(request.POST, user=request.user)
+        if form.is_valid():
+            if form.data.get('submit_publish'):
+                page = form.save_page(
+                    site_slug=site_slug,
+                    language_code=language_code,
+                    page_id=page_id,
+                    publish=True
+                )
+                if page_id:
+                    messages.success(request, _('Page was successfully published.'))
+                else:
+                    page_id = page.id
+                    messages.success(request, _('Page was successfully created and published.'))
+            elif form.data.get('submit_archive'):
+                page = form.save_page(
+                        site_slug=site_slug,
+                        language_code=language_code,
+                        page_id=page_id,
+                        archived=True
+                    )
+                if page_id:
+                    messages.success(request, _('Page was successfully saved.'))
+                else:
+                    page_id = page.id
+                    messages.success(request, _('Page was successfully created.'))
+            else:
+                page = form.save_page(
+                    site_slug=site_slug,
+                    language_code=language_code,
+                    page_id=page_id,
+                    publish=False
+                )
+                if page_id:
+                    messages.success(request, _('Page was successfully saved.'))
+                else:
+                    page_id = page.id
+                    messages.success(request, _('Page was successfully created.'))
+        else:
+            messages.error(request, _('Errors have occurred.'))
+        if page_id:
+            return redirect('edit_page', **{
+                'page_id': page_id,
+                'site_slug': site_slug,
+                'language_code': language_code,
+            })
+        return redirect('new_page', **kwargs)
+
+
+@login_required
+def archive_page(request, page_id, site_slug, language_code):
+    page = Page.objects.get(id=page_id)
+    page.public = False
+    page.archived = True
+    page.save()
+
+    messages.success(request, _('Page was successfully archived.'))
+
+    return redirect('pages', **{
+                'site_slug': site_slug,
+                'language_code': language_code,
+            })
+
+
+@login_required
+def restore_page(request, page_id, site_slug, language_code):
+    page = Page.objects.get(id=page_id)
+    page.archived = False
+    page.save()
+
+    messages.success(request, _('Page was successfully restored.'))
+
+    return redirect('pages', **{
+                'site_slug': site_slug,
+                'language_code': language_code,
+            })
---
+++ backend/cms/views/pages/sbs_page_form.py
@@ -0,0 +1,126 @@
+"""
+Form for creating a page object and page translation object
+"""
+
+import sys
+from django import forms
+from django.utils.text import slugify
+from django.utils.translation import ugettext as _
+
+from ...models import Page, PageTranslation, Site, Language
+from ..general import POSITION_CHOICES
+
+
+class SBSPageForm(forms.ModelForm):
+    """
+    DjangoForm Class, that can be rendered to create deliverable HTML
+
+    Args:
+        forms : Defines the form as an Model form related to a database object
+    """
+
+    position = forms.ChoiceField(choices=POSITION_CHOICES)
+    parent = forms.ModelChoiceField(queryset=Page.objects.all(), required=False)
+    icon = forms.ImageField(required=False)
+    PUBLIC_CHOICES = (
+        (True, _('Public')),
+        (False, _('Private'))
+    )
+    public = forms.ChoiceField(choices=PUBLIC_CHOICES)
+
+    class Meta:
+        model = PageTranslation
+        fields = ['title', 'text', 'status']
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super(SBSPageForm, self).__init__(*args, **kwargs)
+
+    def save_page(self, site_slug, language_code, page_id=None, publish=False, archived=False):
+        """Function to create or update a page
+
+            page_id ([Integer], optional): Defaults to None.
+            publish ([Boolean], optional): Defaults to False. Flag for changing publication status
+                                           via publish button.
+
+            Returns:
+                page: the created or updated page
+        """
+
+        slug = slugify(self.cleaned_data['title'])
+        page_translation = PageTranslation.objects.filter(
+            page__id=page_id,
+            language__code=language_code
+        ).first()
+        # make sure the slug derived from the title is unique
+        if (
+                (
+                    (
+                        # page is created
+                        not page_id
+                        or
+                        # translation is created
+                        not page_translation
+                    )
+                    or
+                    # slug has changed
+                    page_translation.slug != slug
+                )
+                and
+                # the new slug already exists
+                PageTranslation.objects.filter(slug=slug).exists()
+        ):
+            old_slug = slug
+            i = 1
+            while True:
+                i += 1
+                slug = old_slug + '-' + str(i)
+                if not PageTranslation.objects.filter(slug=slug).exists():
+                    break
+
+        # TODO: version, active_version
+
+        if publish:
+            self.cleaned_data['public'] = True
+
+        # TODO: version, active_version
+        if page_id:
+            # save page
+            page = Page.objects.get(id=page_id)
+            page.icon = self.cleaned_data['icon']
+            if archived:
+                page.public = False
+                page.archived = True
+            else:
+                page.public = self.cleaned_data['public']
+            page.save()
+            page.move_to(self.cleaned_data['parent'], self.cleaned_data['position'])
+        else:
+            # create page
+            page = Page.objects.create(
+                icon=self.cleaned_data['icon'],
+                site=Site.objects.get(slug=site_slug),
+            )
+            page.move_to(self.cleaned_data['parent'], self.cleaned_data['position'])
+
+        if page_translation:
+            # save page translation
+            page_translation.slug = slug
+            page_translation.title = self.cleaned_data['title']
+            page_translation.text = self.cleaned_data['text']
+            page_translation.status = self.cleaned_data['status']
+            page_translation.save()
+        else:
+            # create page translation
+            page_translation = PageTranslation.objects.create(
+                slug=slug,
+                title=self.cleaned_data['title'],
+                text=self.cleaned_data['text'],
+                status=self.cleaned_data['status'],
+                language=Language.objects.get(code=language_code),
+                public=self.cleaned_data['public'],
+                page=page,
+                creator=self.user
+            )
+
+        return page
